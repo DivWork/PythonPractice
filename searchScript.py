51785920ef90b74161a1e110b470a98169f02cbb
Divyesh Pal <divyeshpal07@gmail.com>
2022-06-01 20:29:04 +0530

    Create searchScript.py

---
+++ searchScript.py
@@ -0,0 +1,37 @@
+import glob, os
+
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            if fullPath.endswith(".php"):
+                allFiles.append(fullPath)
+            else:
+                continue
+    return allFiles
+
+
+def main():
+    paths = getListOfFiles("/Users/divyesh.pal/Documents/open-backend-v2/")
+    string = 'Redis::'
+    for path in paths:
+        for file in glob.glob(path):
+            fileOpened = open(file,'r')
+            flag = 0
+            index = 0
+            for line in fileOpened:
+                index += 1
+                if string in line:
+                    flag = 1
+                    break
+            if flag == 0:
+                continue
+            else:
+                print(file, string, "found in line index : "+str(index))
+            fileOpened.close()
+
+main()
